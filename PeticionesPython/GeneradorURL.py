0035c3fe4a509f3d146274310ebd82ece04bc973
javierasping <javierasping@gmail.com>
2023-05-09 13:10:57 +0200

    Parte 1

---
+++ PeticionesPython/GeneradorURL.py
@@ -0,0 +1,28 @@
+import hashlib
+import time
+
+PUBLIC_KEY = "1bf075d536f284d8d6c923c4b425be90"
+PRIVATE_KEY = "94c1286e33750006ed249aac0b114366a3a41694"
+
+timestamp = str(time.time())
+
+hash_string = timestamp + PRIVATE_KEY + PUBLIC_KEY
+
+hash = hashlib.md5(hash_string.encode()).hexdigest()
+
+url = f"https://gateway.marvel.com/v1/public/characters?apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
+url2 = f"https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=I&orderBy=name&limit=5&apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
+url3 = f"https://gateway.marvel.com:443/v1/public/series?apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
+url4 = f"https://gateway.marvel.com:443/v1/public/stories?limit=1&apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
+
+print(url)
+print(url2)
+print(url3)
+print(url4)
+
+# https://gateway.marvel.com/v1/public/characters?apikey=1bf075d536f284d8d6c923c4b425be90&hash=71fc8f2c795e90c55924a4de0c858519&ts=1683568166.7326257
+# https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=I&orderBy=name&limit=5&apikey=1bf075d536f284d8d6c923c4b425be90&hash=71fc8f2c795e90c55924a4de0c858519&ts=1683568166.7326257
+# https://gateway.marvel.com:443/v1/public/series?apikey=1bf075d536f284d8d6c923c4b425be90&hash=71fc8f2c795e90c55924a4de0c858519&ts=1683568166.7326257
+# https://gateway.marvel.com:443/v1/public/characters?limit=1&apikey=1bf075d536f284d8d6c923c4b425be90&hash=71fc8f2c795e90c55924a4de0c858519&ts=1683568166.7326257
+#https://gateway.marvel.com:443/v1/public/comics?limit=1&apikey=1bf075d536f284d8d6c923c4b425be90&hash=e563065eef6867ef34c141a04500e0b6&ts=1683568390.0844688
+# https://gateway.marvel.com:443/v1/public/stories?limit=1&apikey=1bf075d536f284d8d6c923c4b425be90&hash=9318f4a5dbf8d484a9ca6d7982a2306f&ts=1683570269.7742023
--- PeticionesPython/Peticion1.py
+++ PeticionesPython/Peticion1.py
@@ -1,26 +1,11 @@
-import hashlib
-import time
+import requests
 
-# Your API key and private key
-PUBLIC_KEY = "1bf075d536f284d8d6c923c4b425be90"
-PRIVATE_KEY = "94c1286e33750006ed249aac0b114366a3a41694"
+url = "https://gateway.marvel.com/v1/public/characters?apikey=1bf075d536f284d8d6c923c4b425be90&hash=0ac16158f12e2734c5e67838045eded3&ts=1683278024.2311842"
 
-# Generate a timestamp
-timestamp = str(time.time())
+respuesta = requests.get(url)
 
-# Combine the timestamp, private key, and API key
-hash_string = timestamp + PRIVATE_KEY + PUBLIC_KEY
+respuesta_json= respuesta.json()
 
-# Generate an MD5 hash of the resulting string
-hash = hashlib.md5(hash_string.encode()).hexdigest()
-
-# Include the hash and API key in your request URL
-url = f"https://gateway.marvel.com/v1/public/characters?apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
-url2 = f"https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=I&orderBy=name&limit=5&apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
-url3 = f"https://gateway.marvel.com:443/v1/public/series?apikey={PUBLIC_KEY}&hash={hash}&ts={timestamp}"
-#https://gateway.marvel.com:443/v1/public/series?apikey=1bf075d536f284d8d6c923c4b425be90
-#https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=I&orderBy=name&limit=5&apikey=1bf075d536f284d8d6c923c4b425be90
-print(url)
-print(url2)
-print(url3)
+for i in respuesta_json['data']['results']:
+    print(i['name'])
 
